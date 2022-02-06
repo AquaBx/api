1051770e2ec32734e3180e44231821c679cd84bd
Tom Chauvel <tom.chauvel@hotmail.com>
2022-02-06 23:15:34 +0100

    ru test 1

---
+++ api/ru.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const jsdom = require("jsdom");
+
+async function request(url){
+  const resp = await axios.get(url);
+  return resp.data;
+};
+
+async function scrap(url){
+  var req = await request(url)
+  const dom = new jsdom.JSDOM(req);
+  const document = dom.window.document
+
+  let lis2 = document.querySelectorAll("#menu-repas .slides > li")
+
+  let liste2 = []
+  for (let tr of lis2) {
+      let nlist = []
+
+      var title = tr.querySelectorAll("h3")[0].innerText.split("Menu du ")[1]
+      var date = new Date(title)
+      var datenow = Date.now()
+      if(date.getTime()+72900000 > datenow){
+          nlist.push(title)
+          let div = tr.querySelectorAll(".content > div")
+
+          for (let content of div){
+              nlist.push(content.innerHTML)
+          }
+          liste2.push(nlist)
+      }
+  }
+  return liste2
+}
+
+module.exports = async function (req, res) {
+    try{
+      var url = "https://www.crous-rennes.fr/restaurant/resto-u-"+req.query.q
+      var result = await parse(url)
+      res.status(200).json(result);
+    } 
+    catch (error) {
+      return res.status(200).json(error);
+    }
+}
